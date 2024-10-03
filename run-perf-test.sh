c0cbdcbb937754ae44e86fb2b7d2444955eef3de
Ava Hahn <a.hahn@f5.com>
2024-10-03 16:37:56 -0700

    update docker tooling
    Signed-off-by: Ava Hahn <a.hahn@f5.com>

--- run-perf-test.sh
+++ run-perf-test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-docker build -t unit-perf .
+if [ find . -type d -empty -iname "unit" ]; then 
+    git submodule update --init
+fi
+
+docker build -t unit-perf --network host .
 docker-compose up
